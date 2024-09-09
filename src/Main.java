b3404dfadce277be4cf9af5780d109a6379bb8a9
Chris Roy <chrisroy604@gmail.com>
2024-09-09 10:48:40 -0400

    Created new main file

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String test = new String("ppp");
+        FizzBuzz.doFizzBuzz(15);
+        FizzBuzz.main();
+
+    }
+
+}
